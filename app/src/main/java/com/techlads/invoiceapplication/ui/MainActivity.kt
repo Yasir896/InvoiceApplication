28486941582e3431e25c98b33e6134af05b58aca
Yasir Ghafar <yasir896@gmail.com>
2022-08-01 06:47:02 -0700

    BaseFragment Implemented

--- app/src/main/java/com/techlads/invoiceapplication/ui/MainActivity.kt
+++ app/src/main/java/com/techlads/invoiceapplication/ui/MainActivity.kt
@@ -2,13 +2,26 @@ package com.techlads.invoiceapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.techlads.invoiceapplication.R
+import com.techlads.invoiceapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.hide()
+
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+            val navController = navHostFragment.navController
     }
+
 }
